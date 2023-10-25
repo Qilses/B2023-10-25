96b1e814f53d5e7d503083b5c3dd2cb32e4e2033
Qilses <robin.eriksson@elev.ga.ntig.se>
2023-10-25 11:21:40 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        /* double[] Komp1 = new double[4];
+        int [] Komp2 = {5,10,20,50,100,200,500};
+        for (int i=0; i<Komp1.length; i++){
+            System.out.println(Komp1[i]);
+        }
+        for (int i=0; i<Komp2.length; i++){
+            System.out.println(Komp2[i]);
+        }
+        */
+        double[] Komp1 = {1/1,1/2,1/3,1/4,1/5,1/6,1/7,1/8,1/9,1/10};
+        for (int i=0; i<Komp1.length; i++) {
+            System.out.println(Komp1[i]);
+        }
+
+    }
+}
